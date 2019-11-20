bd1cba911c78178896747eecd69996315607bc87
LM-Randomize <32624643+LM-Randomize@users.noreply.github.com>
2019-11-20 01:20:37 -0300

    Competencias
    Competencias

---
+++ src/test/kotlin/unq/deportes/CompetitionTest.kt
@@ -0,0 +1,33 @@
+package unq.deportes
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
+import unq.deportes.controller.CompetitionController
+
+@SpringBootTest
+class CompetitionTest {
+
+    @Autowired
+    lateinit var competitionController: CompetitionController
+
+    @Test
+    fun `WHEN findAll() is called SHOULD return a non-empty collection and OK httpstate`(){
+        competitionController.save("CompTest", 2019, "UNQ")
+        val result = competitionController.findAll()
+        assertEquals(HttpStatus.OK, result.statusCode)
+        assertNotNull(result.body?.firstOrNull())
+    }
+
+    @Test
+    fun `WHEN findDetailsOfId() is called with an id THEN SHOULD return CompetitionDetails of the id and OK httpstate`(){
+        competitionController.save("CompTest", 2019, "UNQ")
+        val result = competitionController.findDetailsOfId(1L)
+        assertEquals(HttpStatus.OK, result.statusCode)
+        assertEquals(1L, result.body?.getId())
+    }
+
+}
