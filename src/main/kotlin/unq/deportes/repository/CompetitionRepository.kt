37bd2d363bb47a743f8daab00c5ef42345eade04
LM-Randomize <32624643+LM-Randomize@users.noreply.github.com>
2019-12-04 10:03:43 -0300

    Cambios varios team, match, jugadores, etc.

--- src/main/kotlin/unq/deportes/repository/CompetitionRepository.kt
+++ src/main/kotlin/unq/deportes/repository/CompetitionRepository.kt
@@ -9,6 +9,6 @@ import unq.deportes.model.CompetitionPositions
 
 @Repository
 interface CompetitionRepository : CrudRepository<Competition, Long> {
-    @Query("select new unq.deportes.model.CompetitionPositions(cp.position, t) from Team t, CompetitionDetailItem cp where cp.competitionId.id = :id and t.id = cp.team order by t.id")
+    @Query("select new unq.deportes.model.CompetitionPositions(cp.position, cp.points, t) from Team t, CompetitionDetailItem cp where cp.competitionId.id = :id and t.id = cp.team order by cp.position")
     fun findDetailItemsOf(@Param("id") id: Long):List<CompetitionPositions>
 }
